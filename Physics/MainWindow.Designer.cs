cf5f26e8d3825d405a005321f9a199348feef82d
scrat98 <scrat98@mail.ru>
2017-05-28 00:30:46 +0300

    First structure

---
+++ Physics/MainWindow.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Physics
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputContainer = new System.Windows.Forms.TableLayoutPanel();
+            this.HeightContainer = new System.Windows.Forms.TableLayoutPanel();
+            this.HeightLabel = new System.Windows.Forms.Label();
+            this.HeightInput = new System.Windows.Forms.RichTextBox();
+            this.MassContainer = new System.Windows.Forms.TableLayoutPanel();
+            this.MassLabel = new System.Windows.Forms.Label();
+            this.MassInput = new System.Windows.Forms.RichTextBox();
+            this.WindowContainer = new System.Windows.Forms.TableLayoutPanel();
+            this.InputContainer.SuspendLayout();
+            this.HeightContainer.SuspendLayout();
+            this.MassContainer.SuspendLayout();
+            this.WindowContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputContainer
+            // 
+            this.InputContainer.ColumnCount = 2;
+            this.InputContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.InputContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.InputContainer.Controls.Add(this.HeightContainer, 1, 0);
+            this.InputContainer.Controls.Add(this.MassContainer, 0, 0);
+            this.InputContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputContainer.Location = new System.Drawing.Point(3, 3);
+            this.InputContainer.Name = "InputContainer";
+            this.InputContainer.RowCount = 5;
+            this.InputContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.81481F));
+            this.InputContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.81482F));
+            this.InputContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.81482F));
+            this.InputContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+            this.InputContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.InputContainer.Size = new System.Drawing.Size(1000, 294);
+            this.InputContainer.TabIndex = 0;
+            // 
+            // HeightContainer
+            // 
+            this.HeightContainer.ColumnCount = 2;
+            this.HeightContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.HeightContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.HeightContainer.Controls.Add(this.HeightLabel, 0, 0);
+            this.HeightContainer.Controls.Add(this.HeightInput, 1, 0);
+            this.HeightContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HeightContainer.Location = new System.Drawing.Point(503, 3);
+            this.HeightContainer.Name = "HeightContainer";
+            this.HeightContainer.RowCount = 1;
+            this.HeightContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.HeightContainer.Size = new System.Drawing.Size(494, 37);
+            this.HeightContainer.TabIndex = 2;
+            // 
+            // HeightLabel
+            // 
+            this.HeightLabel.AutoSize = true;
+            this.HeightLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HeightLabel.Location = new System.Drawing.Point(3, 0);
+            this.HeightLabel.Name = "HeightLabel";
+            this.HeightLabel.Size = new System.Drawing.Size(94, 37);
+            this.HeightLabel.TabIndex = 0;
+            this.HeightLabel.Text = "H0(m)";
+            this.HeightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HeightInput
+            // 
+            this.HeightInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HeightInput.Location = new System.Drawing.Point(103, 3);
+            this.HeightInput.Name = "HeightInput";
+            this.HeightInput.Size = new System.Drawing.Size(388, 31);
+            this.HeightInput.TabIndex = 1;
+            this.HeightInput.Text = "";
+            // 
+            // MassContainer
+            // 
+            this.MassContainer.ColumnCount = 2;
+            this.MassContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.MassContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MassContainer.Controls.Add(this.MassLabel, 0, 0);
+            this.MassContainer.Controls.Add(this.MassInput, 1, 0);
+            this.MassContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MassContainer.Location = new System.Drawing.Point(3, 3);
+            this.MassContainer.Name = "MassContainer";
+            this.MassContainer.RowCount = 1;
+            this.MassContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.MassContainer.Size = new System.Drawing.Size(494, 37);
+            this.MassContainer.TabIndex = 0;
+            // 
+            // MassLabel
+            // 
+            this.MassLabel.AutoSize = true;
+            this.MassLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MassLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MassLabel.Location = new System.Drawing.Point(3, 0);
+            this.MassLabel.Name = "MassLabel";
+            this.MassLabel.Size = new System.Drawing.Size(94, 37);
+            this.MassLabel.TabIndex = 0;
+            this.MassLabel.Text = "Mass(kg)";
+            this.MassLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MassInput
+            // 
+            this.MassInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MassInput.Location = new System.Drawing.Point(103, 3);
+            this.MassInput.Name = "MassInput";
+            this.MassInput.Size = new System.Drawing.Size(388, 31);
+            this.MassInput.TabIndex = 1;
+            this.MassInput.Text = "";
+            // 
+            // WindowContainer
+            // 
+            this.WindowContainer.ColumnCount = 1;
+            this.WindowContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.WindowContainer.Controls.Add(this.InputContainer, 0, 0);
+            this.WindowContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WindowContainer.Location = new System.Drawing.Point(0, 0);
+            this.WindowContainer.Name = "WindowContainer";
+            this.WindowContainer.RowCount = 2;
+            this.WindowContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.WindowContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.WindowContainer.Size = new System.Drawing.Size(1006, 753);
+            this.WindowContainer.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 753);
+            this.Controls.Add(this.WindowContainer);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "MainWindow";
+            this.Text = "Physics";
+            this.InputContainer.ResumeLayout(false);
+            this.HeightContainer.ResumeLayout(false);
+            this.HeightContainer.PerformLayout();
+            this.MassContainer.ResumeLayout(false);
+            this.MassContainer.PerformLayout();
+            this.WindowContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel InputContainer;
+        private System.Windows.Forms.TableLayoutPanel MassContainer;
+        private System.Windows.Forms.Label MassLabel;
+        private System.Windows.Forms.RichTextBox MassInput;
+        private System.Windows.Forms.TableLayoutPanel WindowContainer;
+        private System.Windows.Forms.TableLayoutPanel HeightContainer;
+        private System.Windows.Forms.Label HeightLabel;
+        private System.Windows.Forms.RichTextBox HeightInput;
+    }
+}
+
---
+++ Physics/MainWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using System.Windows.Forms;
+using LiveCharts;               //Core of the library
+using LiveCharts.Wpf;           //The WPF controls
+using LiveCharts.WinForms;      //the WinForm wrappers
+
+namespace Physics
+{
+    public partial class MainWindow : Form
+    {
+        bool OK = false;         // if all values are correct
+
+        private double mass;     // mass of the body
+        private double defaultH; // start height
+        private double g;        // g
+        private double angle;    // [0; pi/2]
+
+        private double v1;      // first Boundary value
+        private double v2;      // second Boundary value
+
+        private double k1;      // first interval value
+        private double k2;      // second inteval value
+        private double k3;      // third interval value
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
